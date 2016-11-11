d77230a4105c103157dc074c243130b347543c9f
NC-studio <2481496@gmail.com>
2016-11-11 22:46:16 +0600

    Version 0.02

--- app/src/main/java/com/example/a24814/qzalog/FormFragment.java
+++ app/src/main/java/com/example/a24814/qzalog/FormFragment.java
@@ -22,29 +22,20 @@ public class FormFragment extends Fragment {
 
     private FrameLayout regionSpinner;
 
-    private final String DEFAULT_VALUE = "Не важно";
-
-
     private View view;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.form,
                 container, false);
-
         initView();
-
         return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-
-
     }
 
     private void initView(){
@@ -55,16 +46,12 @@ public class FormFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
-
-
     }
 
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
         JSONObject formRegion = ((BaseFile) getActivity().getApplication()).getFormRegion();
-        Log.d("testio REGION", String.valueOf(formRegion));
         try {
             String value = formRegion.getString("name");
             if(value != null){
@@ -75,7 +62,6 @@ public class FormFragment extends Fragment {
         }catch (JSONException e){
             Log.d(TAG, e.getMessage());
         }
-
     }
 
 
